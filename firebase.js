a4acbef03612137442d99c1b845dc85657d164b2
Cleyton Duda Macedo <maccleyton@gmail.com>
2026-03-23 19:11:15 -0400

    update-firebase

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA9CAztrSnIQ3RiYyyB94b6k_twEqsOxLY",
+    authDomain: "nexus-editor.firebaseapp.com",
+    projectId: "nexus-editor",
+    storageBucket: "nexus-editor.firebasestorage.app",
+    messagingSenderId: "106015148590",
+    appId: "1:106015148590:web:c81cf337af0e8f0e6eadbf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
